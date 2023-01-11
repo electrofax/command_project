cdd926ec36ad1de0dc6230822e5a79b8cfcc3d6f
Sergey Shirobokov <49881602+Hronius@users.noreply.github.com>
2023-01-11 12:32:28 +0300

    form_release4

--- src/views/Form.jsx
+++ src/views/Form.jsx
@@ -14,129 +14,182 @@ function Form({ user }) {
             поэтому ты можешь закрывать пункты в удобном для тебя порядке.
           </p>
         </div>
-        <h5>Нужно подготовить твое рабочее место:</h5>
         <form method="" action="">
-          <ul>
-            <li>
-              <p>
-                <input type="checkbox" name="pass_key" value="1680" onChange />
-                Наставник выдал мне пропуск
-              </p>
-            </li>
-            <li>
-              <p>
-                <input type="checkbox" name="wifi" value="1680" onChange />
-                Наставник сообщил пароль от Wi-Fi
-              </p>
-            </li>
-            <li>
-              <p>
-                <input type="checkbox" name="laptop" value="1680" onChange />
-                Системный администратор выдал мне ноутбук
-              </p>
-            </li>
-            <li>
-              <p>
-                <input
-                  type="checkbox"
-                  name="email_pass"
-                  value="1680"
-                  onChange
-                />
-                Системный администратор выдал мне доступы к почте
-              </p>
-            </li>
-            <li>
-              <p>
-                <input
-                  type="checkbox"
-                  name="stationery"
-                  value="1680"
-                  onChange
-                />
-                Ты отправил(-а) офис-менеджеру на почту список необходимой для
-                тебя канцелярии
-              </p>
-            </li>
-          </ul>
-        </form>
-        <h5>Важно познакомиться с коллегами: :</h5>
-        <form method="" action="">
-          <ul>
-            <li>
-              <p>
-                <input type="checkbox" name="meet_head" value="1680" onChange />
-                Ты познакомился (-ась) со своим руководителем
-              </p>
-            </li>
-            <li>
-              <p>
-                <input
-                  type="checkbox"
-                  name="chattochat"
-                  value="1680"
-                  onChange
-                />
-                Ты написал(-а) сообщение в командный чат
-              </p>
-            </li>
-            <li>
-              <p>
-                <input
-                  type="checkbox"
-                  name="three_names_check"
-                  value="1680"
-                  onChange
-                />
-                Напиши имена трех твоих коллег по отделу:
-                <input name="three_names"></input>
-              </p>
-            </li>
-          </ul>
-        </form>
-        <h5>Важно пройти оформление в отделе кадров: :</h5>
-        <form method="" action="">
-          <ul>
-            <li>
-              <p>
-                <input
-                  type="checkbox"
-                  name="skans_of_docks"
-                  value="1680"
-                  onChange
-                />
-                Ты отправил (-а) сканы документы на оформление в отдел кадров
-              </p>
-            </li>
-            <li>
-              <p>
-                <input
-                  type="checkbox"
-                  name="com_secret"
-                  value="1680"
-                  onChange
-                />
-                Ты подписал (-а) соглашение о коммерческой тайне
-              </p>
-            </li>
-          </ul>
-        </form>
-        <h5>А теперь самое время работать: :</h5>
-        <form method="" action="">
-          <ul>
-            <li>
-              <p>
-                <input type="checkbox" name="#" value="1680" onChange />
-                Получи свою первую задачу у руководителя
-              </p>
-            </li>
-            <li>
-              <p>
-                <input type="checkbox" name="#" value="1680" onChange />
-                Создай подпись в почте по корпоративному шаблону
-              </p>
-            </li>
-          </ul>
+          <h5>Нужно подготовить твое рабочее место:</h5>
+          <div>
+            <ul>
+              <li>
+                <div class="form-group">
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="pass_key"
+                      class="real-checkbox"
+                    />
+                    <span class="custom-checkbox"></span>
+                    &nbsp;Наставник выдал мне пропуск
+                  </label>
+                </div>
+              </li>
+              <li>
+                <div class="form-group">
+                  <label>
+                    <input type="checkbox" name="wifi" class="real-checkbox" />
+                    <span class="custom-checkbox"></span>
+                    &nbsp;Наставник сообщил пароль от Wi-Fi
+                  </label>
+                </div>
+              </li>
+              <li>
+                <div class="form-group">
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="laptop"
+                      class="real-checkbox"
+                    />
+                    <span class="custom-checkbox"></span>
+                    &nbsp;Системный администратор выдал мне ноутбук
+                  </label>
+                </div>
+              </li>
+              <li>
+                <div class="form-group">
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="email_pass"
+                      class="real-checkbox"
+                    />
+                    <span class="custom-checkbox"></span>
+                    &nbsp;Системный администратор выдал мне доступы к почте
+                  </label>
+                </div>
+              </li>
+              <li>
+                <div class="form-group">
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="stationery"
+                      class="real-checkbox"
+                    />
+                    <span class="custom-checkbox"></span>
+                    &nbsp;Ты отправил(-а) офис-менеджеру на почту список
+                    необходимой для тебя канцелярии
+                  </label>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <h5>Важно познакомиться с коллегами: :</h5>
+          <div method="" action="">
+            <ul>
+              <li>
+                <div class="form-group">
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="meet_head"
+                      class="real-checkbox"
+                    />
+                    <span class="custom-checkbox"></span>
+                    &nbsp;Ты познакомился (-ась) со своим руководителем
+                  </label>
+                </div>
+              </li>
+              <li>
+                <div class="form-group">
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="chattochat"
+                      class="real-checkbox"
+                    />
+                    <span class="custom-checkbox"></span>
+                    &nbsp;Ты написал(-а) сообщение в командный чат
+                  </label>
+                </div>
+              </li>
+              <li>
+                <div class="form-group">
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="stationery"
+                      class="real-checkbox"
+                    />
+                    <span class="custom-checkbox"></span>
+                    &nbsp;Напиши имена трех твоих коллег по отделу:&nbsp;
+                    <input name="three_names"></input>
+                  </label>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <h5>Важно пройти оформление в отделе кадров: :</h5>
+          <div method="" action="">
+            <ul>
+              <li>
+                <div class="form-group">
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="skans_of_docks"
+                      class="real-checkbox"
+                    />
+                    <span class="custom-checkbox"></span>
+                    &nbsp;Ты отправил (-а) сканы документы на оформление в отдел
+                    кадров
+                  </label>
+                </div>
+              </li>
+              <li>
+                <div class="form-group">
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="com_secret"
+                      class="real-checkbox"
+                    />
+                    <span class="custom-checkbox"></span>
+                    &nbsp;Ты подписал (-а) соглашение о коммерческой тайне
+                  </label>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <h5>А теперь самое время работать: :</h5>
+          <div method="" action="">
+            <ul>
+              <li>
+                <div class="form-group">
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="com_secret"
+                      class="real-checkbox"
+                    />
+                    <span class="custom-checkbox"></span>
+                    &nbsp;Получи свою первую задачу у руководителя
+                  </label>
+                </div>
+              </li>
+              <li>
+                <div class="form-group">
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="com_secret"
+                      class="real-checkbox"
+                    />
+                    <span class="custom-checkbox"></span>
+                    &nbsp;Создай подпись в почте по корпоративному шаблону
+                  </label>
+                </div>
+              </li>
+            </ul>
+          </div>
         </form>
       </div>
     </Layout>
