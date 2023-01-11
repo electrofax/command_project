69d68d68df4b05bd6ee26cd5dc9e81a2cdc972be
Alexander Veshkin <a.veshkin@avtopass.net>
2023-01-11 19:31:05 +0400

    ref

--- src/public/js/fetchForm.js
+++ src/public/js/fetchForm.js
@@ -1,6 +1,8 @@
 import { fetchData, fetchDataGet, el, checkInput } from './helper.js';
 
+//заполнить форму данными (если они есть)
 let url = '/api/form'
+//отправить феч на получение данных
 const resp = await fetchDataGet(url + '/1')
 
 for (let key in resp) {
@@ -12,21 +14,27 @@ for (let key in resp) {
     }
 }
 
+//сохранять каждое изменение формы
 el('#user-form').addEventListener('click', async (target) => {
     const { name } = event.target
 
+    //изменение чекбоксов
     if (checkInput(target.path) && name !== 'three_names') {
         let status = el('input[name="' + name + '"]').checked
         let payload = {};
         payload [ name ] = status
+        //отправить феч
         await fetchData(url, payload, 'PUT');
     }
 
+    //изменение инпута с именами менторов
     if (name === 'three_names') {
+        //сохранить value инпута после того как с него уберут форкус
         el('input[name="three_names"]').onblur = async () => { 
         let value = el('input[name="three_names"]').value
         let payload = {};
         payload [ name ] = value;
+        //отправить феч
         await fetchData(url, payload, 'PUT');
         }
     }
