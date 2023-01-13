2155b0f37345e2d35c3e6b038a5347ab2e679b4a
Sergey Shirobokov <49881602+Hronius@users.noreply.github.com>
2023-01-13 13:56:18 +0300

    releasestylehead

--- src/views/Form.jsx
+++ src/views/Form.jsx
@@ -5,193 +5,229 @@ function Forms({ user, fetchForm, one }) {
   return (
     <LayoutForm user={user} fetchForm={fetchForm}>
       {one ? (
-      <div className="formPage">
-        <div>
-          <h4>
-            Привет, <b>{one.employee_name}</b>! И добро пожаловать в команду Высокогорья!
-
-            Впереди нас ждет интересное путешествие в мир нашей компании, и
-            самым главным проводником будет- <b>{one.mentor_name}</b>. Мы подготовили для тебя
-            чек-лист на первый день. Процесс выполнения будет сохраняться,
-            поэтому ты можешь закрывать пункты в удобном для тебя порядке.
-          </h4>
-        </div>
-        <div className="checkBoxis">
-          <div>
-            <div id="user-form">
-              <h4 className="titleCheckBox">
-                Нужно подготовить твое рабочее место:
-              </h4>
-              <div>
+        <div className="formPage">
+          <div className="formTitle">
+            <h4>
+              Привет, <b>{one.employee_name}</b>! И добро пожаловать в команду
+              Высокогорья! Впереди нас ждет интересное путешествие в мир нашей
+              компании, и самым главным проводником будет-
+              <b>{one.mentor_name}</b>. Мы подготовили для тебя чек-лист на
+              первый день. Процесс выполнения будет сохраняться, поэтому ты
+              можешь закрывать пункты в удобном для тебя порядке.
+            </h4>
+            <hr></hr>
+          </div>
+          <div className="checkBoxis">
+            <div>
+              <div id="user-form">
+                <h4 className="titleCheckBox">
+                  Нужно подготовить твое рабочее место:
+                </h4>
                 <div>
                   <div>
-                    <label className="checkString">
-                      <div className="mylabel">
-                        <input name="pass_key" type="checkbox" id="coding" />
-                        <div className="slidinggroove"></div>
-                      </div>
-                      <p className="strCheckForm">
-                        &nbsp;Наставник выдал мне пропуск
-                      </p>
-                    </label>
-                  </div>
-                  <div>
-                    <label className="checkString">
-                      <div className="mylabel">
-                        <input name="wifi" type="checkbox" id="coding" />
-                        <div className="slidinggroove"></div>
-                      </div>
-                      <p className="strCheckForm">
-                        &nbsp;Наставник сообщил пароль от Wi-Fi{" "}
-                      </p>
-                    </label>
-                  </div>
-                  <div>
-                    <label className="checkString">
-                      <div className="mylabel">
-                        <input name="laptop" type="checkbox" id="coding" />
-                        <div className="slidinggroove"></div>
-                      </div>
-                      <p className="strCheckForm">
-                        &nbsp;Системный администратор выдал мне ноутбук{" "}
-                      </p>
-                    </label>
-                  </div>
-                  <div>
-                    <label className="checkString">
-                      <div className="mylabel">
-                        <input name="email_pass" type="checkbox" id="coding" />
-                        <div className="slidinggroove"></div>
-                      </div>
-                      <p className="strCheckForm">
-                        &nbsp;Системный администратор выдал мне доступы к почте
-                      </p>
-                    </label>
-                  </div>
-                  <div>
-                    <label className="checkString">
-                      <div className="mylabel">
-                        <input name="stationery" type="checkbox" id="coding" />
-                        <div className="slidinggroove"></div>
-                      </div>
-                      <p className="strCheckForm">
-                        &nbsp;Ты отправил(-а) офис-менеджеру на почту список
-                        необходимой для тебя канцелярии
-                      </p>
-                    </label>
+                    <div>
+                      <label className="checkString">
+                        <div className="mylabel">
+                          <input name="pass_key" type="checkbox" id="coding" />
+                          <div className="slidinggroove"></div>
+                        </div>
+                        <div className="strCheckForm">
+                          &nbsp;Наставник выдал мне пропуск
+                        </div>
+                      </label>
+                    </div>
+                    <div>
+                      <label className="checkString">
+                        <div className="mylabel">
+                          <input name="wifi" type="checkbox" id="coding" />
+                          <div className="slidinggroove"></div>
+                        </div>
+                        <p className="strCheckForm">
+                          &nbsp;Наставник сообщил пароль от Wi-Fi{" "}
+                        </p>
+                      </label>
+                    </div>
+                    <div>
+                      <label className="checkString">
+                        <div className="mylabel">
+                          <input name="laptop" type="checkbox" id="coding" />
+                          <div className="slidinggroove"></div>
+                        </div>
+                        <p className="strCheckForm">
+                          &nbsp;Системный администратор выдал мне ноутбук{" "}
+                        </p>
+                      </label>
+                    </div>
+                    <div>
+                      <label className="checkString">
+                        <div className="mylabel">
+                          <input
+                            name="email_pass"
+                            type="checkbox"
+                            id="coding"
+                          />
+                          <div className="slidinggroove"></div>
+                        </div>
+                        <p className="strCheckForm">
+                          &nbsp;Системный администратор выдал мне доступы к
+                          почте
+                        </p>
+                      </label>
+                    </div>
+                    <div>
+                      <label className="checkString">
+                        <div className="mylabel">
+                          <input
+                            name="stationery"
+                            type="checkbox"
+                            id="coding"
+                          />
+                          <div className="slidinggroove"></div>
+                        </div>
+                        <div className="strCheckForm">
+                          &nbsp;Ты отправил(-а) офис-менеджеру на почту список
+                          необходимой для тебя канцелярии
+                        </div>
+                      </label>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <h4 className="titleCheckBox">
-                Важно познакомиться с коллегами:
-              </h4>
-              <div>
+                <hr></hr>
+                <h4 className="titleCheckBox">
+                  Важно познакомиться с коллегами:
+                </h4>
                 <div>
                   <div>
-                    <label className="checkString">
-                      <div className="mylabel">
-                        <input name="meet_head" type="checkbox" id="coding" />
-                        <div className="slidinggroove"></div>
-                      </div>
-                      <p className="strCheckForm">
-                        &nbsp;Ты познакомился (-ась) со своим руководителем
-                      </p>
-                    </label>
-                  </div>
-                  <div>
-                    <label className="checkString">
-                      <div className="mylabel">
-                        <input name="chattochat" type="checkbox" id="coding" />
-                        <div className="slidinggroove"></div>
-                      </div>
-                      <p className="strCheckForm">
-                        &nbsp;Ты написал(-а) сообщение в командный чат
-                      </p>
-                    </label>
-                  </div>
-                  <div>
-                    <label className="checkString">
-                      <div className="mylabel">
-                        <input name="three_names_check" type="checkbox" id="coding" />
-                        <div className="slidinggroove"></div>
-                      </div>
-                      <p className="strCheckForm">
-                        &nbsp;Напиши имена трех твоих коллег по отделу:
-                        <input
-                          className="three_names_input"
-                          name="three_names"
-                        ></input>
-                      </p>
-                    </label>
-                  </div>
-                </div>
-              </div>
-              <h4 className="titleCheckBox">
-                Важно пройти оформление в отделе кадров:
-              </h4>
-              <div method="" action="">
-                <div>
-                  <div>
-                    <label className="checkString">
-                      <div className="mylabel">
-                        <input
-                          name="skans_of_docks"
-                          type="checkbox"
-                          id="coding"
-                        />
-                        <div className="slidinggroove"></div>
-                      </div>
-                      <p className="strCheckForm">
-                        &nbsp;Ты отправил (-а) сканы документы на оформление в
-                        отдел кадров
-                      </p>
-                    </label>
-                  </div>
-                  <div>
-                    <label className="checkString">
-                      <div className="mylabel">
-                        <input name="com_secret" type="checkbox" id="coding" />
-                        <div className="slidinggroove"></div>
-                      </div>
-                      <p className="strCheckForm">
-                        &nbsp;Ты подписал (-а) соглашение о коммерческой тайне
-                      </p>
-                    </label>
+                    <div>
+                      <label className="checkString">
+                        <div className="mylabel">
+                          <input name="meet_head" type="checkbox" id="coding" />
+                          <div className="slidinggroove"></div>
+                        </div>
+                        <p className="strCheckForm">
+                          &nbsp;Ты познакомился (-ась) со своим руководителем
+                        </p>
+                      </label>
+                    </div>
+                    <div>
+                      <label className="checkString">
+                        <div className="mylabel">
+                          <input
+                            name="chattochat"
+                            type="checkbox"
+                            id="coding"
+                          />
+                          <div className="slidinggroove"></div>
+                        </div>
+                        <p className="strCheckForm">
+                          &nbsp;Ты написал(-а) сообщение в командный чат
+                        </p>
+                      </label>
+                    </div>
+                    <div>
+                      <label className="checkString">
+                        <div className="mylabel">
+                          <input
+                            name="three_names_check"
+                            type="checkbox"
+                            id="coding"
+                          />
+                          <div className="slidinggroove"></div>
+                        </div>
+                        <p className="strCheckForm">
+                          &nbsp;Напиши имена трех твоих коллег по отделу:
+                          <input
+                            className="three_names_input"
+                            name="three_names"
+                          ></input>
+                        </p>
+                      </label>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <h4 className="titleCheckBox">А теперь самое время работать:</h4>
-              <div method="" action="">
-                <div>
+                <hr></hr>
+                <h4 className="titleCheckBox">
+                  Важно пройти оформление в отделе кадров:
+                </h4>
+                <div method="" action="">
                   <div>
-                    <label className="checkString">
-                      <div className="mylabel">
-                        <input name="first_task" type="checkbox" id="coding" />
-                        <div className="slidinggroove"></div>
-                      </div>
-                      <p className="strCheckForm">
-                        &nbsp;Получи свою первую задачу у руководителя
-                      </p>
-                    </label>
-                  </div>
-                  <div>
-                    <label className="checkString">
-                      <div className="mylabel">
-                        <input name="email_sign" type="checkbox" id="coding" />
-                        <div className="slidinggroove"></div>
-                      </div>
-                      <p className="strCheckForm">
-                        &nbsp;Создай подпись в почте по корпоративному шаблону
-                      </p>
-                    </label>
+                    <div>
+                      <label className="checkString">
+                        <div className="mylabel">
+                          <input
+                            name="skans_of_docks"
+                            type="checkbox"
+                            id="coding"
+                          />
+                          <div className="slidinggroove"></div>
+                        </div>
+                        <p className="strCheckForm">
+                          &nbsp;Ты отправил (-а) сканы документы на оформление в
+                          отдел кадров
+                        </p>
+                      </label>
+                    </div>
+                    <div>
+                      <label className="checkString">
+                        <div className="mylabel">
+                          <input
+                            name="com_secret"
+                            type="checkbox"
+                            id="coding"
+                          />
+                          <div className="slidinggroove"></div>
+                        </div>
+                        <p className="strCheckForm">
+                          &nbsp;Ты подписал (-а) соглашение о коммерческой тайне
+                        </p>
+                      </label>
+                    </div>
                   </div>
                 </div>
+                <hr></hr>
+                <h4 className="titleCheckBox">
+                  А теперь самое время работать:
+                </h4>
+                <div method="" action="">
+                  <div>
+                    <div>
+                      <label className="checkString">
+                        <div className="mylabel">
+                          <input
+                            name="first_task"
+                            type="checkbox"
+                            id="coding"
+                          />
+                          <div className="slidinggroove"></div>
+                        </div>
+                        <p className="strCheckForm">
+                          &nbsp;Получи свою первую задачу у руководителя
+                        </p>
+                      </label>
+                    </div>
+                    <div>
+                      <label className="checkString">
+                        <div className="mylabel">
+                          <input
+                            name="email_sign"
+                            type="checkbox"
+                            id="coding"
+                          />
+                          <div className="slidinggroove"></div>
+                        </div>
+                        <p className="strCheckForm">
+                          &nbsp;Создай подпись в почте по корпоративному шаблону
+                        </p>
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <hr></hr>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      )  : ( 
+      ) : (
         <h1>Вас не существует</h1>
       )}
     </LayoutForm>
--- src/views/LayoutForm.jsx
+++ src/views/LayoutForm.jsx
@@ -8,50 +8,74 @@ function LayoutForm(props) {
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        
+
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
           crossOrigin="anonymous"
         />
-        <link rel='stylesheet' href='/css/reset.css' />
-        <link rel='stylesheet' href='/css/index.css' />
-        {fetchForm && <script type='module' src='/js/helper.js' />}
-        {fetchForm && <script type='module' src='/js/fetchForm.js' />}
+        <link rel="stylesheet" href="/css/reset.css" />
+        <link rel="stylesheet" href="/cыss/index.css" />
+        <link rel="stylesheet" href="/css/hroni.css" />
+        {fetchForm && <script type="module" src="/js/helper.js" />}
+        {fetchForm && <script type="module" src="/js/fetchForm.js" />}
         <title>Form</title>
       </head>
-      
+
       {user ? (
-      <nav className="navbar navbar-expand-lg nav-color">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container-fluid">
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon" />
+            <a className="navbar-brand" href="#">
+              <img className="logo_img" src="/img/logo.png" alt="Логотип" />
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarColor03"
+              aria-controls="navbarColor03"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+              wfd-invisible="true"
+            >
+              <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="logo">
-              <img src="/img/logo.png" className="img-fluid" alt="..."></img>
-                </li>
-                {/* <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">Ho</a>
-                </li> */}
+            <div className="collapse navbar-collapse" id="navbarColor03">
+              <form className="d-flex">
+
+              </form>
+              <ul className="navbar-nav me-auto">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href='/adopt/all'>
-                    Вернуться
-                    </a>
+                  <a className="nav-link active" href="#">
+                    Создать Лист Адаптации
+                    <span className="visually-hidden">(current)</span>
+                  </a>
                 </li>
-                
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Все Листы Адаптации
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Мои Листы Адаптации
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Все Пользователи
+                  </a>
+                </li>
               </ul>
             </div>
           </div>
         </nav>
-        
-        ) : (
-          <div />
-        )}
-        
-        <body>
+      ) : (
+        <div />
+      )}
+
+      <body>
         {children}
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
